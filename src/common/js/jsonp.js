8035f37d92b165d2f72f03df4bb523fdd49d05cb
fucaobao <466368084@qq.com>
2017-12-05 18:17:31 +0800

    jsonp的应用+轮播图数据抓取

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -4,7 +4,7 @@ export default function jsonp(url, data, option){
     url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
 
     return new Promise((resolve, reject) => {
-        originJSONP(url, option, (err, data) {
+        originJSONP(url, option, (err, data) => {
             if(!err){
                 resolve(data)
             } else {
@@ -18,7 +18,7 @@ function param(data){
     let url = ''
     for(var k in data){
         let value = data[k] !== undefined ? data[k] : ''
-        url += '&{k}=${encodeURIComponent(value)}'
+        url += '&' + k + '=' + encodeURIComponent(value)
     }
     return url ? url.substring(1) : ''
 }
