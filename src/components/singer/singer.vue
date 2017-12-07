37c1a50d6a1f071cf27f266033b5108f617b176f
fucaobao <466368084@qq.com>
2017-12-07 16:21:52 +0800

    歌手数据的处理和Singer类的封装

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,6 +6,11 @@
 <script type="text/ecmascript-6">
   import { getSingerList } from 'api/singer';
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
   export default {
     data() {
       return {
@@ -19,9 +24,56 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if(res.code === ERR_OK) {
-            console.log(res)
+            // console.log(res.data.list)
+            this.singers = res.data.list;
+            console.log(this._normalizeSingerList(this.singers))
           }
         })
+      },
+      _normalizeSingerList(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if(index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fother_name
+            }))
+          }
+          const key = item.Findex
+          if(!map[key]){
+            map[key] = {
+              title: item.Findex,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fother_name
+          }))
+        })
+        // 为了得到有序列表，需要处理map
+        let hot = []
+        let retNum = []
+        let ret = []
+        for(let key in map){
+          let val = map[key]
+          if(val.title.match(/^[a-zA-Z]$/)){
+            ret.push(val)
+          } else if(val.title.match(/^\d$/)){
+            retNum.push(val)
+          } else {
+            hot.push(val)
+          }
+        }
+        ret.sort((a,b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(retNum).concat(ret)
       }
     }
   }
