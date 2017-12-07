503cd7765e3caca96d0921633f143d29f6f9bcb1
fucaobao <466368084@qq.com>
2017-12-07 15:40:24 +0800

    歌手数据的抓取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    歌手页面
+  <div class="single">
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import { getSingerList } from 'api/singer';
+  import {ERR_OK} from 'api/config'
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created () {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if(res.code === ERR_OK) {
+            console.log(res)
+          }
+        })
+      }
+    }
+  }
+
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  .single
+    position: fixed
+    top: 88px
+    botton: 0
+    width: 100%
 </style>
