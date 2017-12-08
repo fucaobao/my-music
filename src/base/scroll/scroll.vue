ad60da6f5003124fc3093fbc4f2bd44bf2c3e612
fucaobao <466368084@qq.com>
2017-12-08 15:27:52 +0800

    完善listview，增加跟随滚动和高亮功能

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -20,6 +20,10 @@
             data: {
                 type: Array,
                 default: null
+            },
+            listenScroll :{
+                type: Boolean,
+                default: false
             }
         },
         data () {
@@ -41,6 +45,12 @@
                     probeType: this.probeType,
                     click: this.click
                 })
+                if(this.listenScroll){
+                    let me = this
+                    me.scroll.on('scroll', (pos) => {
+                        me.$emit('scroll', pos)
+                    })
+                }
             },
             enable() {
                 this.scroll && this.scroll.enable()
