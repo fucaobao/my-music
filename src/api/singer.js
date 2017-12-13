1d673b66ed44ebce17d2a08c5f459960d08cee09
fucaobao <466368084@qq.com>
2017-12-13 18:20:54 +0800

    歌手详情数据的获取

--- src/api/singer.js
+++ src/api/singer.js
@@ -19,3 +19,27 @@ export function getSingerList() {
 
     return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+    const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+    const data = Object.assign({}, commonParams, {
+        channel: 'singer',
+        page: 'list',
+        key: 'all_all_all',
+        pagesize: 100,
+        pagenum: 1,
+        hostUin: 0,
+        format: 'jsonp',
+        singermid: singerId,
+        order: 'listen',
+        begin: 0,
+        num: 100,
+        songstatus: 1,
+        platform: 'yqq',
+        g_tk: 456100168,
+        needNewCode: 0
+    })
+
+    return jsonp(url, data, options)
+}
