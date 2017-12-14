64c7c7854b47a4eef1f10245f10951f4f73aeb52
fucaobao <466368084@qq.com>
2017-12-14 21:58:04 +0800

    1、初始化播放器 2、初步认识mapActions

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="song-list">
         <ul>
-            <li v-for="song in songs" class="item">
+            <li v-for="(song,index) in songs" class="item" @click="selectItem(song,index)">
                 <div class="content">
                     <h2 class="name">{{song.name}}</h2>
                     <p class="desc">{{getDesc(song)}}</p>
@@ -27,6 +27,9 @@
         methods: {
           getDesc(song) {
               return `${song.singer} · ${song.name}`
+          },
+          selectItem(item, index) {
+              this.$emit('select', item, index)
           }
         },
         components: {
