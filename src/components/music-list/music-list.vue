703c556c22c32d8270d18317b59e568832150a11
fucaobao <466368084@qq.com>
2017-12-14 16:36:39 +0800

    完善歌单列表页面

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,27 +1,44 @@
 <template>
     <div class="music-list">
-        <div class="back">
+        <div class="back" @click="back">
             <i class="icon-back"></i>
         </div>
         <h1 class="title" v-html="title"></h1>
         <div class="bg-image" :style="bgStyle" ref="bgImage">
-            <div class="filter"></div>
+            <div class="play-wrapper" v-show="songs.length" ref="playBtn">
+                <div class="play">
+                    <i class="icon-play"></i>
+                    <span class="text">随机播放全部</span>
+                </div>
+            </div>
+            <div class="filter" ref="filter"></div>
         </div>
-        <scroll :data="songs" class="list" ref="list">
+        <div class="bg-layer" ref="layer"></div>
+        <scroll :probeType="probeType" :listen-scroll="listenScroll" @scroll="scroll" :data="songs" class="list" ref="list">
             <div class="song-list-wrapper">
                 <song-list :songs="songs"></song-list>
+            </div>
+            <div v-show="!songs.length" class="loading-container">
+                <loading></loading>
             </div>
         </scroll>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+    import {prefixStyle} from 'common/js/dom'
     import Scroll from 'base/scroll/scroll'
+    import Loading from 'base/loading/loading'
     import SongList from 'base/song-list/song-list'
+
+    const RESERVED_HEIGHT = 40
+    const transform = prefixStyle('transform')
+    const backdrop = prefixStyle('backdrop-filter')
+
     export default {
         data () {
             return {
-
+                scrollY: 0
             }
         },
         props: {
@@ -38,7 +55,12 @@
                 default: ''
             }
         },
+        created () {
+            this.probeType = 3
+            this.listenScroll = true
+        },
         mounted () {
+            console.log(this.$router)
             this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`;
         },
         computed: {
@@ -46,14 +68,72 @@
                 return `background-image:url(${this.bgImage})`
             }
         },
+        methods: {
+            scroll(pos) {
+                this.imageHeight = this.$refs.bgImage.clientHeight
+                this.minTranslateY = - this.imageHeight + RESERVED_HEIGHT
+                this.scrollY = pos.y;
+            },
+            back() {
+                this.$router.back()
+            }
+        },
+        watch: {
+            scrollY(newY) {
+                let translateY = Math.max(this.minTranslateY, newY)
+                let zIndex = 0
+                let scale = 1
+                this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+                const percent = Math.abs(newY / this.imageHeight)
+                if(newY > 0) {
+                    scale = 1 + percent
+                    zIndex = 10
+                } else {
+                    blur = Math.min(20 * percent, 20)
+                }
+                // 用于IOS下图片的高斯模糊
+                this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+                if(newY < this.minTranslateY){
+                    zIndex = 10
+                    this.$refs.bgImage.style.paddingTop = 0
+                    this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+                    this.$refs.playBtn.style.display = 'none'
+                } else {
+                    this.$refs.bgImage.style.paddingTop = '70%'
+                    this.$refs.bgImage.style.height = 0
+                    this.$refs.playBtn.style.display = ''
+                }
+                this.$refs.bgImage.style.zIndex = zIndex
+                this.$refs.bgImage.style[transform] = `scale(${scale})`
+            }
+        },
         components: {
             Scroll,
-            SongList
+            SongList,
+            Loading
         }
     }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+    // transform-origin: top以顶部为基准变换
+    // x-axis
+    // 定义视图被置于 X 轴的何处。可能的值：
+    // left
+    // center
+    // right
+    // length
+    // %
+    // y-axis
+    // 定义视图被置于 Y 轴的何处。可能的值：
+    // top
+    // center
+    // bottom
+    // length
+    // %
+    // z-axis
+    // 定义视图被置于 Z 轴的何处。可能的值：
+    // length
   @import "~common/stylus/variable"
   @import "~common/stylus/mixin"
 
@@ -133,7 +213,6 @@
       top: 0
       bottom: 0
       width: 100%
-      overflow: hidden
       background: $color-background
       .song-list-wrapper
         padding: 20px 30px
