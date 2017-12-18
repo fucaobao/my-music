145dcc18d546cbcddd2432ae2b808a2faaf6d02d
fucaobao <466368084@qq.com>
2017-12-18 23:20:56 +0800

    修复一个translate3d的bug

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -169,7 +169,7 @@
             leave(el, done){
               this.$refs.cdWrapper.style.transition = 'all 0.4s'
               const {x,y,scale} = this._getPosAndScale()
-              this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0)) scale(${scale}`
+              this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
               this.$refs.cdWrapper.addEventListener('transitionend', done)
             },
             afterLeave(el){
