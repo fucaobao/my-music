e972996f44148c693ba4ccdcee2232160e0738c8
fucaobao <466368084@qq.com>
2018-01-03 16:11:14 +0800

    增加路由容器

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -18,6 +18,7 @@
     <div class="search-result" v-show="query">
       <suggest :query="query" @listScroll="blurInput"></suggest>
     </div>
+    <router-view></router-view>
   </div>
 </template>
 
